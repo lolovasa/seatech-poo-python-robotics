813f118ecc5ae11008718aec42d1799ecd3803f3
lolovasa <lolo.vasapolli@hotmail.fr>
2022-01-14 12:03:24 +0100

    exo1_seance1

--- exo1.py
+++ exo1.py
@@ -3,9 +3,45 @@ class Robot():
     __power = False
     __current_speed = 0
     __battery_level = 0
-    __states = ['shutown', 'running']
+    __states = ['shutdown', 'running']
     
         
-    """
-      Give your best code here ( •̀ ω •́ )✧
-    """
+
+    def__init__(self):
+        self.__name = name
+        print('initialisation', format(self.name))
+
+
+    def eteint(self):
+        self.__power=0
+    def allumé(self):
+        self.__power=1
+    
+
+
+    def stop(self):
+        self.__current_speed=0
+    def move(self,speed):
+        if type(speed)== int:
+            self.__current_speed=speed
+    def speed(self):
+        return self.__current_speed
+
+
+
+    def vide (self):
+        self.__battery_level=0
+    def charge(self,charge):
+        if type(charge)== int:
+            self.__battery_level=charge
+    def charge(self):
+        return self.__battery_level
+
+    def shutdown (self):
+        self.power=0
+
+        pass
+
+    def running (self):
+        self.power=1
+        pass
